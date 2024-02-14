0de9db688b6b13dc4cfcb7e5e631688d78099d82
Keith Faunce <149439304+WizzyHarry@users.noreply.github.com>
2024-02-14 13:09:31 -0500

    Update day_4.py

--- day_4.py
+++ day_4.py
@@ -4,13 +4,108 @@
 
 
 
+import cmath
+
+
+print('{0} + {1} = {2}'.format(8, 6, 8 + 6))
+print('{0} - {1} = {2}'.format(8, 6, 8 - 6))
+print('{0} * {1} = {2}'.format(8, 6, 8 * 6))
+print('{0} / {1} = {2}'.format(8, 6, 8 / 6))
+print('{0} % {1} = {2}'.format(8, 6, 8 % 6))
+print('{0} // {1} = {2}'.format(8, 6, 8 // 6))
+print('{0} ** {1} = {2}'.format(8, 6, 8 ** 6))
 
 
 
 
-
-
+sentence = 'You cannot end a sentence with because because because is a conjunction'
 company = 'Coding For All'
+
+
+tabseq = """Name\t\tAge\t\tCountry\t\tCity
+Asabeneh\t250\t\tFinland\t\tHelsinki"""
+print(tabseq)
+
+
+
+widebody = ' Coding For All '
+snip = widebody.strip(' ')
+print(snip)
+
+
+list_list = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+joining = ' # '.join(list_list)
+print(joining)
+
+longstuff = "I am enjoying this challenge. I just wonder what is next."
+print(longstuff)
+
+longbutcutstuf = "I am enjoying this challenge.\nI just wonder what is next."
+print(longbutcutstuf)
+
+
+
+subcomp = 'Coding'
+subtwocomp = 'coding'
+
+if company.startswith(subcomp):
+    print("Yes it does start with substring Coding")
+else:
+    print("nO")
+
+if company.startswith(subtwocomp):
+    print("Yes it does start with the substring coding")
+else:
+    print("L substring")
+
+
+companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+splitting = companies.split(',')
+print(splitting)
+
+
+position = company.rfind('l')
+print(position)
+
+whereat = sentence.find('because')
+print(whereat)
+wherearthou = sentence.rfind('because')
+print(wherearthou)
+chop = sentence.slice('because')
+print(chop)
+
+
+"""I did something wrong here"""
+"""mastersplinter = sentence.slice()
+thesplit = [word for word in mastersplinter if word != 'because']
+remerge = ' '.join(thesplit)
+print(remerge)"""
+
+
+
+placement = company.index('C')
+print(placement)
+
+placing = company.index('F')
+print(placing)
+
+
+abbrev = ''.join(word[0] for word in company.split(' '))
+print(abbrev)
+
+two = 'Python for All'
+shorten = ''.join(word[0] for word in two.split(' '))
+print(shorten)
+
+print(company[0])
+print(company[-1])
+print(company[10])
+
+
+
+splitter = company.split(' ')
+print(splitter)
+
 
 replacing = company.replace("Coding", "python")
 print(replacing)
@@ -18,13 +113,6 @@ print(replacing)
 newline = "Python for Everyone"
 newreplace = newline.replace("for Everyone", "for All")
 print(newreplace)
-
-
-
-
-
-
-
 
 
 """find"""
