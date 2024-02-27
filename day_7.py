6b953fc8629de3768e397d2cfa08693b9b21abe6
Keith Faunce <149439304+WizzyHarry@users.noreply.github.com>
2024-02-26 19:08:32 -0500

    Update day_7.py

--- day_7.py
+++ day_7.py
@@ -17,7 +17,7 @@ print(it_companies)
 
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
-age = [22, 19, 24, 25, 26, 24, 25, 24]
+
 C = A.union(B)
 print(C)
 
@@ -31,4 +31,21 @@ BA = B.union(A)
 print(AB, BA) #they produced the same output
 del A
 del B #delete em
+
+
+
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+age_list = list(age)
+print(len(age_list))
+print(len(age)) #they're the same size
+"""A string is a piece of length of text that can be declared
+A tuple, list and set are all methods for storing data. 
+A set is a randomly ordered list that can be modified
+"""
+
+sentence = "I am a teacher and I love to inspire and teach people."
+words = sentence.split()
+seperated_words = set(words)
+num_words = len(seperated_words)
+print(num_words)
 
