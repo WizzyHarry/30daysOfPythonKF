47d82bf29844d120a62276cb87cee2d2662fe48a
Keith Faunce <149439304+WizzyHarry@users.noreply.github.com>
2024-02-28 12:09:23 -0500

    Add files via upload

---
+++ day_9.py
@@ -0,0 +1,70 @@
+
+print("Input your grade")
+grade = int(input())
+if grade >= 80 and grade <= 100:
+    print("You got an A")
+elif grade >= 70 and grade <= 89:
+    print("You got a B")
+elif grade >= 60 and grade <= 69:
+    print("You don't deserve this C")
+elif grade >= 50 and grade <= 59:
+    print("Drop out, D")
+else:
+    print("Off to China"
+
+
+
+
+
+
+
+
+
+
+
+
+print("Input two numbers, A and B, press enter after each")
+A = int(input())
+B = int(input())
+if A > B:
+    print(A, "is greater than ", B)
+elif B > A:
+    print(B, "is greater than ", A)
+else:
+    print("Numbers are equal")
+
+
+
+
+
+
+
+
+"""
+print("Enter your age")
+your_age = int(input())
+print("Enter my age")
+my_age = int(input())
+
+if your_age > my_age:
+    print("You're older than me!")
+elif my_age > your_age:
+    print("I'm older than you!")
+else:
+    print("We are the same age!")
+
+
+
+
+print('qeuifh')
+
+age = int(input())
+
+if age >= 18:
+    print("You are old enough to drive")
+else:
+    num = 18 - age
+    print("You need ",num, "more years to learn to drive")
+
+
+"""
